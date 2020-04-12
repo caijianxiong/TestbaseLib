47d19c2a723f638aa042042e35fe3d67d6003a08
DESKTOP-KBAM2ST\菜菜 <1153448695@qq.com>
2020-04-12 22:54:15 +0800

    add utils

---
+++ app/src/main/java/com/example/mydemo/TimeUtil.java
@@ -0,0 +1,8 @@
+package com.example.mydemo;
+
+public class TimeUtil {
+
+    public static String getTime(){
+        return "hello";
+    }
+}
---
+++ baselib/src/main/java/cjx/liyueyun/baselib/base/mvp/utils/StringUtil.java
@@ -0,0 +1,8 @@
+package cjx.liyueyun.baselib.base.mvp.utils;
+
+public class StringUtil {
+
+    public static String getString(){
+        return "hello";
+    }
+}
