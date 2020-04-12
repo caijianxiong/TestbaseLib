23ffb1c6f3c6c8ff410f8f0bec26b421e26a037c
DESKTOP-KBAM2ST\菜菜 <1153448695@qq.com>
2020-04-12 22:23:49 +0800

    add layout_welcome.xml

--- app/src/main/java/com/example/mydemo/WelcomeActivity.java
+++ app/src/main/java/com/example/mydemo/WelcomeActivity.java
@@ -8,6 +8,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.layout_welcome);
     }
 }
